c73c17343cc75f24b1c28409bdd7382a2d58ee89
DQCoding1 <diegoabquintero@gmail.com>
2023-03-15 17:44:27 -0500

    creating logic and structure of toggle mode

--- javascript/header/header.js
+++ javascript/header/header.js
@@ -1,4 +1,9 @@
 const profession = document.querySelector(".header__profession");
+const body = document.querySelector("body")
+const modeContainer = document.querySelector(".header__mode") 
+const darkMode = document.querySelector(".header__dark")
+const toggleMode = document.querySelector(".header__toggleMode")
+const lightMode = document.querySelector(".header__light")
 
 const moveProfession = () => {
   const currentScrollY = window.scrollY;
@@ -14,3 +19,26 @@ const moveProfession = () => {
 };
 
 window.addEventListener("scroll", moveProfession);
+
+const changeMode = (e) => {
+  if(e.target.classList.contains("header__dark")){
+    body.classList.add("darkMode")
+    body.classList.remove("lightMode")
+  } else if (e.target.classList.contains("header__toggleMode")){
+    if(body.classList.contains("darkMode")){
+      body.classList.add("lightMode")
+      body.classList.remove("darkMode")
+    } else {
+      body.classList.add("darkMode")
+      body.classList.remove("lightMode")
+    }
+  } else if (e.target.classList.contains("header__light")){
+    body.classList.add("lightMode")
+    body.classList.remove("darkMode")
+
+  }
+}
+
+modeContainer.addEventListener("click", changeMode)
+
+
