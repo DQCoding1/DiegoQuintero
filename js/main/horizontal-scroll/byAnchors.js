4d8d5dacba731374f07e050b799c02fc18fb29a4
DQCoding1 <diegoabquintero@gmail.com>
2023-10-06 16:24:09 -0500

    add skills section

--- js/main/horizontal-scroll/byAnchors.js
+++ js/main/horizontal-scroll/byAnchors.js
@@ -1,34 +1,42 @@
 const anchors = document.querySelectorAll("a");
-const sectionHome = document.querySelector("[data-section='home']");
-const sectionAbout = document.querySelector("[data-section='about']");
-const sectionProjects = document.querySelector("[data-section='projects']");
-const sectionContact = document.querySelector("[data-section='contact']");
+const homeSection = document.querySelector("[data-section='home']");
+const aboutSection = document.querySelector("[data-section='about']");
+const projectsSection = document.querySelector("[data-section='projects']");
+const contactSection = document.querySelector("[data-section='contact']");
+const skillsSection = document.querySelector("[data-section='skills']");
 
 const goToSection = (e) => {
     switch (e.target.dataset.go_to) {
-        case sectionHome.dataset.section: {
-            sectionHome.scrollIntoView({
+        case homeSection.dataset.section: {
+            homeSection.scrollIntoView({
                 behavior: "smooth",
                 inline: "center",
             });
             break;
         }
-        case sectionAbout.dataset.section: {
-            sectionAbout.scrollIntoView({
+        case aboutSection.dataset.section: {
+            aboutSection.scrollIntoView({
                 behavior: "smooth",
                 inline: "center",
             });
             break;
         }
-        case sectionProjects.dataset.section: {
-            sectionProjects.scrollIntoView({
+        case skillsSection.dataset.section: {
+            skillsSection.scrollIntoView({
                 behavior: "smooth",
                 inline: "center",
             });
             break;
         }
-        case sectionContact.dataset.section: {
-            sectionContact.scrollIntoView({
+        case projectsSection.dataset.section: {
+            projectsSection.scrollIntoView({
+                behavior: "smooth",
+                inline: "center",
+            });
+            break;
+        }
+        case contactSection.dataset.section: {
+            contactSection.scrollIntoView({
                 behavior: "smooth",
                 inline: "center",
             });
