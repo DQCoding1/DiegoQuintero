04b064fe397a087742d6e0dd9ca064fdb643900d
DQCoding1 <diegoabquintero@gmail.com>
2023-10-06 12:58:39 -0500

    header html and css

--- javascript/presentationAnimation/presentationAnimation.js
+++
@@ -1,7 +0,0 @@
-const presentation = document.querySelector(".presentationAnimation");
-
-const removePresentation = () => {
-  setTimeout(() => presentation.style.display = "none", 5000);
-};
-
-removePresentation();
