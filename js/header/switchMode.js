19a0a2e95be25f56604955573f0addc224f1a7bf
DQCoding1 <diegoabquintero@gmail.com>
2023-10-13 00:55:34 -0500

    handle light mode

--- js/header/switchMode.js
+++ js/header/switchMode.js
@@ -1,5 +1,6 @@
 const switchEl = document.querySelector(".header__switch");
 const switchBtnEl = document.querySelector(".header__btn-switch");
+import handleBrandImages from "../main/brand-image/handleBrandImage.js"
 const switchImgs = document.querySelectorAll(".header__img-switch");
 const body = document.querySelector("body")
 
@@ -9,6 +10,8 @@ const switchMode = () => {
     switchBtnEl.classList.toggle("header__btn-switch--light")
     switchImgs[0].classList.toggle("header__img-switch--active")
     switchImgs[1].classList.toggle("header__img-switch--active")
+
+    handleBrandImages()
 }
 
 switchEl.addEventListener("click", switchMode)
