161827dcfd529997cc79cc885e6b488ef598dc67
DQCoding1 <diegoabquintero@gmail.com>
2023-10-17 21:36:02 -0500

    improve home scroll element classes

--- js/header/switchMode.js
+++ js/header/switchMode.js
@@ -1,17 +1,17 @@
+import handleHomeBrandImages from "../main/brand-image/handleBrandImage.js";
 const switchEl = document.querySelector(".header__switch");
-const switchBtnEl = document.querySelector(".header__btn-switch");
-import handleBrandImages from "../main/brand-image/handleBrandImage.js"
-const switchImgs = document.querySelectorAll(".header__img-switch");
-const body = document.querySelector("body")
+const switchBtn = document.querySelector(".switch__btn");
+const switchIcons = document.querySelectorAll(".switch__icon");
+const body = document.querySelector("body");
 
 const switchMode = () => {
-    body.classList.toggle("dark-mode")
-    body.classList.toggle("light-mode")
-    switchBtnEl.classList.toggle("header__btn-switch--light")
-    switchImgs[0].classList.toggle("header__img-switch--active")
-    switchImgs[1].classList.toggle("header__img-switch--active")
+    body.classList.toggle("dark-mode");
+    body.classList.toggle("light-mode");
+    switchBtn.classList.toggle("switch__btn--light");
+    switchIcons[0].classList.toggle("switch__icon--visible");
+    switchIcons[1].classList.toggle("switch__icon--visible");
 
-    handleBrandImages()
-}
+    handleHomeBrandImages();
+};
 
-switchEl.addEventListener("click", switchMode)
+switchEl.addEventListener("click", switchMode);
--- js/main/brand-image/handleBrandImage.js
+++ js/main/brand-image/handleBrandImage.js
@@ -1,20 +1,19 @@
-const homeBrandImgs = document.querySelectorAll(".home__brandImg")
-const brandDarkDesktop = document.getElementById("brand-darkmode-desktop")
-const brandlightDesktop = document.getElementById("brand-lightmode-desktop")
+const homeBrandImgs = document.querySelectorAll(".home__brandImg");
+const brandDarkDesktop = document.getElementById("brand-darkmode-desktop");
+const brandlightDesktop = document.getElementById("brand-lightmode-desktop");
 
-export default function handleBrandImages(){
-    homeBrandImgs.forEach((item) =>{
-        if (item.classList.contains("home__brandImg--active")){
-            item.classList.remove("home__brandImg--active")
+export default function handleHomeBrandImages() {
+    homeBrandImgs.forEach((item) => {
+        if (item.classList.contains("home__brandImg--active")) {
+            item.classList.remove("home__brandImg--active");
         }
-    })
+    });
 
-    if (document.body.classList.contains("light-mode")){
-        brandlightDesktop.classList.add("home__brandImg--active")
-    }
-    if (document.body.classList.contains("dark-mode")){
-        brandDarkDesktop.classList.add("home__brandImg--active")
+    if (document.body.classList.contains("light-mode")) {
+        brandlightDesktop.classList.add("home__brandImg--active");
     }
 
-    
+    if (document.body.classList.contains("dark-mode")) {
+        brandDarkDesktop.classList.add("home__brandImg--active");
+    }
 }
